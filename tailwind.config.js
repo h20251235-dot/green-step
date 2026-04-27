2dd54747d47500234b0db7fc8f9ca132c6962d44
h20251235-dot <h20251235@glab.hallym.ac.kr>
2026-04-27 21:25:00 +0900

    Add custom color 'mystic-deep' to Tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'mystic-deep': '#7C3AED',
+      }
+    },
   },
   plugins: [],
 }
